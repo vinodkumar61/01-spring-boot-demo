02bfd4e6b6a411fd71e88a4ae7c2e6b58d08ef66
Vinod Kumar <vinodkumarnkt61@gmail.com>
2022-11-24 13:59:21 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 01-spring-boot-demo
+ 01-spring-boot-demo
---
+++ src/main/java/com/geeks/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,18 @@
+package com.geeks.springboot.demo.mycoolapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+	// expose "/" that returns "HelloWorld"
+	@GetMapping("/")
+	public String sayHello() {
+		
+		return "Hello World Time on server is " + LocalDateTime.now();
+	}
+	
+}
